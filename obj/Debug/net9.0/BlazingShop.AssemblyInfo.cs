ac1f3cbccda057f896f28e81913eca61a1c60489
Adriano Grevizirsky <grevizirsky@gmail.com>
2025-06-10 23:53:28 -0300

    Adicionando Entity Framework

--- obj/Debug/net9.0/BlazingShop.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52cd6c1749534df66f7b769aac5b120a1f728e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9a3cbbf938a1a5a467303b8533078d40accd75")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
