ab9a3cbbf938a1a5a467303b8533078d40accd75
Adriano Grevizirsky <grevizirsky@gmail.com>
2025-06-10 23:49:29 -0300

    adicionando Router, listando dados e separando em componentes

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+namespace BlazingShop.Models;
+
+public class Category
+{
+    public Category()
+    {
+        
+    }
+    
+    public Category(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
