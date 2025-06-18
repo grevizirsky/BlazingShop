40f6661d6221c684e76a2faa85e1f6359cdce3b8
Adriano Grevizirsky <grevizirsky@gmail.com>
2025-06-17 23:45:30 -0300

    customizando erros

--- obj/Debug/net9.0/BlazingShop.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08e3c93835522588a4e4e4e24efdcde26261583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65a19a857634d62fa85f28b884c1c8553018ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
